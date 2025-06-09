60e1d60a40f8d50b9877b8bd6b3900d3433a1fe9
EngrIBGIT <ibrahim5322022@hotmail.com>
2025-06-09 20:06:04 +0100

    .git..updated6

--- api/main.py
+++ api/main.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI
 
+from mangum import Mangum
+
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+
+handler = Mangum(app=app)  # This is the handler for AWS Lambda
